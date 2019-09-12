76f0517916ec4760a10255d804ff50528e875133
UTAmmar <ammarsheikh@sbcglobal.net>
2019-09-11 22:33:59 -0500

    Readded DB, need to fix async task submitting issue

--- ChatroomApp/server/Driver.java
+++ ChatroomApp/server/Driver.java
@@ -1,12 +1,11 @@
 package server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
-import java.nio.channels.CompletionHandler;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,36 +13,31 @@ import java.util.concurrent.Future;
 
 public class Driver{
 
-	static int clientCount = 0;
-	static boolean running = true;
-	
+	private static int clientCount = 0;
+
 	public static void main(String[] args) throws IOException {
 		
-		//DataBase clientDB = DataBase.createDB(new File("ClientData.ser"));
-		
-		
-		System.out.println("Main Thread " + Thread.currentThread());
+		//Create and init "DataBase", which reads deserializes/serilizes info from given file
+		DataBase.createDB(new File("ClientData.ser"));
 		
 		//Create Threadpool which will handle all async messaging tasks
 		int numberOfThreads = 10;
 		ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
 		AsynchronousChannelGroup group = AsynchronousChannelGroup.withThreadPool(pool);
-		
-				
-		System.out.println("This is the async server");
+			
 		AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(group);
 		server.bind(new InetSocketAddress(4040));
+		System.out.println("Java App server running...");
 	
-
-		//This next part is essentially blocking, but I need it this way to 
-		//create an asycnsocketchannel
+		//This next part is essentially blocking, but its a simple way to ensure a
+		//I get an AsyncSocketChannel asap.
 		try {
 			while(true){
-			Future<AsynchronousSocketChannel> client = server.accept();
-			AsynchronousSocketChannel clientSocket = client.get();
-			AppHandler clientHandler = new AppHandler(clientSocket);
-			pool.submit(clientHandler);
-			System.out.println("Client Accepted: " + ++clientCount);
+				Future<AsynchronousSocketChannel> client = server.accept();
+				AsynchronousSocketChannel clientSocket = client.get();
+				AppHandler clientHandler = new AppHandler(clientSocket);
+				pool.submit(clientHandler);
+				System.out.println("Client Accepted: " + ++clientCount);
 			}
 		} catch (InterruptedException  | ExecutionException e) {
 			e.printStackTrace();
