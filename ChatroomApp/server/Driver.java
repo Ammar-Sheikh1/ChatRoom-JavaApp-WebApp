805f8016f098a35cc672820ca35d8418ab5a1213
UTAmmar <ammarsheikh@sbcglobal.net>
2019-09-11 22:35:31 -0500

    Added comments

--- ChatroomApp/server/Driver.java
+++ ChatroomApp/server/Driver.java
@@ -17,18 +17,20 @@ public class Driver{
 
 	public static void main(String[] args) throws IOException {
 		
+		//TODO: Reintegrate WebApp/WebSockets into Async design
+
 		//Create and init "DataBase", which reads deserializes/serilizes info from given file
 		DataBase.createDB(new File("ClientData.ser"));
-		
+
 		//Create Threadpool which will handle all async messaging tasks
 		int numberOfThreads = 10;
 		ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
 		AsynchronousChannelGroup group = AsynchronousChannelGroup.withThreadPool(pool);
-			
+
 		AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(group);
 		server.bind(new InetSocketAddress(4040));
 		System.out.println("Java App server running...");
-	
+
 		//This next part is essentially blocking, but its a simple way to ensure a
 		//I get an AsyncSocketChannel asap.
 		try {
@@ -43,6 +45,7 @@ public class Driver{
 			e.printStackTrace();
 		} 
 		
+
 	}
 
 
